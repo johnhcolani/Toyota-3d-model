2aa512046acd5e5ac1f87810f04c035d559a0e24
johnhabibpour <johnhcolani@gmail.com>
2024-06-17 17:00:31 -0400

    Added Page view and smooth page indicator

---
+++ lib/models/on_boarding_model.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class OnBoardingModel {
+
+  String title;
+  String description;
+
+
+  OnBoardingModel(
+
+      this.title,
+      this.description,
+     );
+}
